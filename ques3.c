b39b39d41df08ae1e046bf065b64d58e6408aabd
thakralvansh0009 <thakralvansh0009@gmail.com>
2025-08-28 13:33:16 +0530

    Create ques3.c

---
+++ ques3.c
@@ -0,0 +1,10 @@
+//Predict the Output of the following program  
+# include <stdio.h>
+int main() { 
+int i; 
+int arr[5] = {1}; 
+for (i = 0; i < 5; i++) 
+printf("%d",arr[i]); 
+return 0; 
+}
+// it will print 1 and at remaining places any garbage value will be stored.
